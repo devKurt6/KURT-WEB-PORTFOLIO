d48abbea11c65be0e5a8aa9ce83f3457f12d9e8b
devkurt6 <kurtdecena41@gmail.com>
2025-10-25 21:36:27 +0800

    oct 25

--- script.js
+++ script.js
@@ -16,3 +16,47 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   updateIcon();
 });
+
+/************************** */
+document.addEventListener("DOMContentLoaded", () => {
+  const images = document.querySelectorAll(".zoom-in");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      entry.target.classList.remove("animate-zoom-in");
+      entry.target.classList.remove("animate-zoom-out");
+      if (entry.isIntersecting) {
+        // Add animation when visible
+        
+        entry.target.classList.add("animate-zoom-in");
+      } else {
+        // Remove animation when hidden (so it can replay)
+        entry.target.classList.add("animate-zoom-out");
+      }
+    });
+  }, { threshold: 0.3 }); // Trigger at 20% visibility
+
+  images.forEach(img => observer.observe(img));
+});
+
+/********************************************************************** */
+document.addEventListener("DOMContentLoaded", () => {
+  const textElements = document.querySelectorAll(".slide-in-elliptic-top-fwd");
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      entry.target.classList.remove("bounce-top");
+      
+      if (entry.isIntersecting) {
+        // Add animation when visible
+        
+        entry.target.classList.add("bounce-top");
+      } else {
+        // Remove animation when hidden (so it can replay)
+        entry.target.classList.remove("bounce-top");
+      }
+    });
+  }, { threshold: 0.3 }); // Trigger at 20% visibility
+
+  textElements.forEach(txt => observer.observe(txt));
+});
